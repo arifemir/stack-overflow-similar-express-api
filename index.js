808e740e812a39ed78a577253fdd0bc692bcfdc3
aearslanturk <arifarslanturk@gmail.com>
2020-07-22 15:43:37 +0300

    db connection;

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ dotenv.config({
   path: './config/env/config.env'
 })
 
+require('./helpers/database/connectDatebase')();
+
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
 app.use('/api', require('./routes'))
 
 app.listen(PORT, () => {
-  console.log('served to '+PORT + process.env.NODE_ENV)
+  console.log('served to '+PORT + " " + process.env.NODE_ENV)
 })
