ffa9b95981d96ab537070f05ae4c4e5f230ce50f
aearslanturk <arifarslanturk@gmail.com>
2020-07-22 04:16:44 +0300

    env variables; +dotenv

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: './config/env/config.env'
+})
 
 const app = express();
-
-
 const PORT = 5000 || process.env.PORT;
+
 
 app.get('/',(req,res) => {
   res.send('hi');
 })
 
 app.listen(PORT,() => {
-  console.log('served to '+PORT);
+  console.log('served to '+PORT + process.env.NODE_ENV);
 })
