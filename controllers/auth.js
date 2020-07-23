47b289235202e07fc0e09a14e4e926bad0be93a4
aearslanturk <arifarslanturk@gmail.com>
2020-07-23 20:43:43 +0300

    user create

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,14 @@
-register = (req, res, next) => {
-  res.status(200).json({success: true})
+const User = require('../models/user')
+
+register = async (req, res, next) => {
+  
+  const user = await User.create({
+    name:'name',
+    email:'emasdil@gmail.com ',
+    password:'password'
+  })
+
+  res.status(200).json({success: true, data: user})
 }
 
 module.exports = {
