6a062eb1c48f96a6b4220e1feb57e7ef2c58c266
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2020-08-23 00:48:30 +0300

    added morgan for log

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const path = require('path')
 const chalk = require('chalk')
+const morgan = require('morgan')
 
 dotenv.config({
 	path: './config/env/config.env'
@@ -14,6 +15,7 @@ const PORT = 5000 || process.env.PORT
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan('dev'))
 
 app.use('/api', require('./routes'))
 
