c8c7010c4390a6c52b62fb12f7e8a6749d6ad35c
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2020-09-08 00:20:07 +0300

    token sent email

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,6 +9,8 @@ const {
 	comparePassword
 } = require('../helpers/authorization/inputHelpers')
 
+const sendEmail = require('../helpers/libraries/sendEmail')
+
 const register = asyncErrorWrapper(async (req, res, next) => {
 	const { name, email, password, role } = req.body
 	const user = await User.create({ name, email, password, role })
@@ -69,22 +71,39 @@ const imageUpload = asyncErrorWrapper(async (req, res, next) => {
 })
 
 const forgotPassword = asyncErrorWrapper(async (req, res, next) => {
-	const resetEmail = req.body.email;
+	const resetEmail = req.body.email
 
-	const user = await User.findOne({email: resetEmail})
+	const user = await User.findOne({ email: resetEmail })
 
-	if(!user) {
+	if (!user) {
 		return next(new CustomError('There is no user with that email', 400))
 	}
 
-	user.getResetPasswordTokenFromUser()
+	const resetPasswordToken = user.getResetPasswordTokenFromUser()
 	await user.save()
-	
-	res.json({
-		success: true,
-		message: 'Token Sent To Your Email'
-	})
 
+	const resetPasswordUrl = `http://localhost:5000/api/auth/resetpassword?resetPasswordToken=${resetPasswordToken}`
+
+	const emailTemplate = `<h3>Reset Your Password</h3>
+	<p>This <a href="${resetPasswordUrl}" target="_blank">will expire in 1 hour</a></p>`
+
+	try {
+		await sendEmail({
+			from: process.env.SMTP_USER,
+			to: resetEmail,
+			subject: 'Reset Your Password',
+			html: emailTemplate
+		})
+		return res.status(200).json({
+			success: true,
+			message: 'Token Sent To Your Email'
+		})
+	} catch (err) {
+		user.resetPasswordToken = undefined
+		user.resetPasswordExpire = undefined
+		await user.save()
+		return next(new CustomError('Email Could Not Be Sent', 500))
+	}
 })
 
 module.exports = {
